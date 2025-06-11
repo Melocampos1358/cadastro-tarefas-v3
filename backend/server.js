56c0c0620a593c6e641651e64befd8a38a9400d1
Melocampos1358 <nikolaskampos1234@gmail.com>
2025-06-11 11:37:04 -0300

    Subir alterações de conexão com o banco de dados

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,40 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const tarefaRoutes = require("./routes/tarefaRoutes");
+// const sequelize = require("./database/db");
+// const Tarefa = require("./models/Tarefa");
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(tarefaRoutes);
+
+// sequelize.sync().then(() => {
+//   app.listen(3000, () => {
+//     console.log("Servidor rodando em http://localhost:3000");
+//   });
+// });
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const tarefaRoutes = require("./routes/tarefaRoutes");
 const sequelize = require("./database/db");
-const Tarefa = require("./models/Tarefa");
+const app = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
+
+//rotas
 app.use(tarefaRoutes);
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+//sincronizar o banco de dados e iniciar o servidor
+sequelize.sync()
+  .then(() => {
+      console.log("Banco de dados sincronizado e servidor rodando em http://localhost:3000");
+      app.listen(3000, () => {
+        console.log("Servidor rodando em http://localhost:3000");
+    });
+    
+  }).catch((error) => {
+    console.error("Erro ao sincronizar o banco de dados:", error);
   });
-});
